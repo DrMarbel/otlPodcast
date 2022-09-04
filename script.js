792327887eefd2e8375171029f284320662f2215
Dr. Marbel <martinbelt753@gmail.com>
2022-09-03 22:26:58 -0500

    added a neat little activity circle

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ var app = new Vue({
       .then((response) => {
           console.log(response);
 
-          document.getElementById("user").innerHTML = response.data.members[5].username + ": " + response.data.members[5].status;
+          var user = document.getElementById("user");
+          var status = response.data.members[5].status;
+
+          user.innerHTML = response.data.members[5].username + ": " + response.data.members[5].status;
+
           document.getElementById("drMarb").src = response.data.members[5].avatar_url;
+          var dot = document.getElementById("dot");
+          
+          if (status == "online")
+          {
+            dot.className = "position-absolute top-300 start-100 translate-middle p-2 bg-success border border-light rounded-circle"
+          }
+          else if (status == "idle")
+          {
+            dot.className = "position-absolute top-300 start-100 translate-middle p-2 bg-warning border border-light rounded-circle"
+          }
       })
   }
 })
